c552a38d735a2295f16a4a217e24284f06649d22
Silvia Lozano <sylvilozano@gmail.com>
2021-02-23 15:10:41 +0100

    Texto

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2> Estamos aprendiendo a hacer unit Tests - El drama de los tests</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
